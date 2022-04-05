192f59b35c70b306bfe6304124af0e813b5ac7b3
Zhao KK <kikakkz@hotmail.com>
2022-04-05 22:28:14 +0800

    Add announcement

--- src/components/announcement/Announcement.vue
+++ src/components/announcement/Announcement.vue
@@ -1,13 +1,40 @@
 <template>
-  <div class='background'>
+  <div v-if='!hidden && announcement' class='background'>
     <div class='content'>
       <div class='announcement row'>
         <q-icon class='icon' name='volume_up' />
-        For test announcement
+        {{ announcement.Title }}
       </div>
     </div>
   </div>
 </template>
+
+<script setup lang='ts'>
+import { computed, ref, onMounted, onUnmounted } from 'vue'
+import { useMailboxStore, Announcement } from 'npool-cli-v2'
+
+const mailbox = useMailboxStore()
+const hidden = computed(() => mailbox.Announcements.length === 0)
+
+const index = ref(0)
+const ticker = ref(-1)
+const announcement = ref(undefined as unknown as Announcement)
+
+onMounted(() => {
+  ticker.value = window.setInterval(() => {
+    index.value %= mailbox.Announcements.length
+    announcement.value = !hidden.value ? mailbox.Announcements[index.value] : undefined as unknown as Announcement
+    index.value += 1
+  }, 3000)
+})
+
+onUnmounted(() => {
+  if (ticker.value >= 0) {
+    window.clearInterval(ticker.value)
+  }
+})
+
+</script>
 
 <style lang='sass' scoped>
 .background
