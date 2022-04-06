07ef00725b98b422f9d5d0873b8c25ee8cad9111
Zhao KK <kikakkz@hotmail.com>
2022-04-06 23:12:16 +0800

    Support responsive header

--- src/components/header/HeaderTools.vue
+++ src/components/header/HeaderTools.vue
@@ -1,46 +1,58 @@
 <template>
-  <q-tabs
-    dense
-    v-model='curTab'
-    narrow-indicator
-    no-caps
-    inline-label
-    outside-arrows
-    mobile-arrows
-    indicator-color='primary'
-    class='no-shadow text-dark-text'
-  >
-    <div class='row' v-for='tab in tabs' :key='tab.Name'>
-      <q-tab
-        :name='tab.Name'
-        :label='$t(tab.Label)'
-        @click='onTabClick(tab)'
+  <div class='row'>
+    <q-tabs
+      dense
+      v-model='curTab'
+      narrow-indicator
+      no-caps
+      inline-label
+      outside-arrows
+      mobile-arrows
+      :style='{"max-width": tabWidth + "px"}'
+      indicator-color='primary'
+      class='no-shadow text-dark-text tabs'
+    >
+      <div class='row' v-for='tab in tabs' :key='tab.Name'>
+        <q-tab
+          :name='tab.Name'
+          :label='$t(tab.Label)'
+          @click='onTabClick(tab)'
+        />
+      </div>
+    </q-tabs>
+    <div class='row'>
+      <q-btn
+        dense
+        flat
+        :label='$t("MSG_SIGNIN")'
+        class='btn btn-round'
       />
+      <q-btn
+        dense
+        flat
+        :label='$t("MSG_SIGNUP")'
+        class='btn btn-round btn-filled'
+      />
+      <q-separator
+        vertical
+        class='separator'
+      />
+      <LangSwitcher />
+      <q-resize-observer @resize='onResize' />
     </div>
-    <q-btn
-      dense
-      flat
-      :label='$t("MSG_SIGNIN")'
-      class='btn btn-round'
-    />
-    <q-btn
-      dense
-      flat
-      :label='$t("MSG_SIGNUP")'
-      class='btn btn-round btn-filled'
-    />
-    <q-separator
-      vertical
-      class='separator'
-    />
-    <LangSwitcher />
-    <q-resize-observer @resize='onResize' />
-  </q-tabs>
+  </div>
 </template>
 
 <script setup lang='ts'>
-import { ref, defineAsyncComponent, defineEmits } from 'vue'
+import { ref, defineAsyncComponent, defineProps, toRef, computed } from 'vue'
 import { Size } from '../../types/size'
+
+interface Props {
+  maxWidth: number
+}
+
+const props = defineProps<Props>()
+const maxWidth = toRef(props, 'maxWidth')
 
 const LangSwitcher = defineAsyncComponent(() => import('src/components/lang/LangSwitcher.vue'))
 
@@ -85,9 +97,11 @@ const onTabClick = (tab: Tab) => {
   console.log('TODO: click', tab)
 }
 
-const emit = defineEmits<{(e: 'update:width', width: number): void}>()
+const opWidth = ref(0)
+const tabWidth = computed(() => maxWidth.value - opWidth.value)
+
 const onResize = (size: Size) => {
-  emit('update:width', size.width)
+  opWidth.value = size.width
 }
 
 </script>
@@ -101,5 +115,9 @@ const onResize = (size: Size) => {
   padding: 0 $mini-item-width
 
 .separator
-  margin: 4px 20px 4px 10px
+  margin: 4px 16px 4px 10px
+
+.tabs
+  min-width: 200px
+  margin-right: 10px
 </style>
--- src/components/header/MainHeader.vue
+++ src/components/header/MainHeader.vue
@@ -2,17 +2,13 @@
   <div class='header row'>
     <Logo v-model:width='logoWidth' class='logo' />
     <q-space />
-    <HeaderTools v-if='showFull' v-model:width='toolsWidth' />
-    <span v-else>...</span>
+    <HeaderTools :max-width='toolbarWidth - logoWidth' />
     <q-resize-observer @resize='onResize' />
   </div>
-  <div>{{ logoWidth }} 1</div>
-  <div>{{ toolsWidth }} 2</div>
-  <div>{{ toolbarWidth }} 3</div>
 </template>
 
 <script setup lang='ts'>
-import { defineAsyncComponent, ref, computed } from 'vue'
+import { defineAsyncComponent, ref } from 'vue'
 import { Size } from '../../types/size'
 
 const Logo = defineAsyncComponent(() => import('src/components/logo/Logo.vue'))
@@ -24,15 +20,13 @@ const onResize = (size: Size) => {
 }
 
 const logoWidth = ref(0)
-const toolsWidth = ref(0)
-const showFull = computed(() => logoWidth.value + toolsWidth.value <= toolbarWidth.value)
 
 </script>
 
 <style lang='sass' scoped>
 .header
   min-height: $medium-item-height
-  margin-top: $medium-item-height
+  margin-top: $small-item-height
 
 .logo
   height: $medium-item-height
